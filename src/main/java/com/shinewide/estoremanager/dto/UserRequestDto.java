ed98ea3e6be0f28c81d90cff0298d9c5d8f1df3e
K-CHANHO <cksgh1565@gmail.com>
2025-02-25 16:30:55 +0900

    User 도메인 추가

---
+++ src/main/java/com/shinewide/estoremanager/dto/UserRequestDto.java
@@ -0,0 +1,19 @@
+package com.shinewide.estoremanager.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserRequestDto {
+
+    @Email(message = "유효한 이메일을 입력하세요.")
+    @NotBlank(message = "이메일은 필수 입력 항목입니다.")
+    private String email;
+
+    @NotBlank(message = "비밀번호는 필수 입력 항목입니다.")
+    private String password;
+
+}
