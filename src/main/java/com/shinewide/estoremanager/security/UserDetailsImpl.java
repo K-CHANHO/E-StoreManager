df7362fc68cbb815ae39b6bf12b6900d8e3a98c8
Chanho Kim <cksgh1565@gmail.com>
2025-03-13 00:04:54 +0900

    [ING] User 권한처리 수정 중

--- src/main/java/com/shinewide/estoremanager/security/UserDetailsImpl.java
+++ src/main/java/com/shinewide/estoremanager/security/UserDetailsImpl.java
@@ -7,6 +7,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.Collections;
+import java.util.List;
 
 @Getter
 public class UserDetailsImpl implements UserDetails {
@@ -19,7 +20,7 @@ public class UserDetailsImpl implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return Collections.emptyList();
+        return List.of(() -> "ROLE_" + user.getRole());
     }
 
     @Override
